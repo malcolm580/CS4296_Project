b7b22ffa3836ba4ee0346221db5ece43337d0a00
malcolm580 <youmalcolm580@gmail.com>
2019-07-16 23:21:24 +0800

    Update app.js

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -1,14 +1,3 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(8080, function () {
+        console.log('Express started on http://localhost:' + 8080 + '; press Ctrl-C to terminate.');
+    });

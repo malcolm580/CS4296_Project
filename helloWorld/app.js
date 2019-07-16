c1b075157e66ccdeadced11b4144f5e6fe5834bc
malcolm580 <youmalcolm580@gmail.com>
2019-07-16 23:28:10 +0800

    Update app.js

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -1,3 +1,14 @@
-app.listen(8080, function () {
-        console.log('Express started on http://localhost:' + 8080 + '; press Ctrl-C to terminate.');
-    });
+var http = require("http");
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');

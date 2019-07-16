871d9860af0d16a4a0d351ebb66a6a2269c4b107
malcolm580 <youmalcolm580@gmail.com>
2019-07-16 23:36:09 +0800

    Update app.js

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -1,14 +1,14 @@
-var http = require("http");
+const http = require('http');
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
